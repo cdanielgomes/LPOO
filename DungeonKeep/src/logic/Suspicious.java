9d962f7d21b3737fd61ad70380b53df6a813d083
Unknown <up201603404@fe.up.pt>
2018-04-03 19:55:57 +0100

    Suspicious its well now

--- DungeonKeep/src/logic/Suspicious.java
+++ DungeonKeep/src/logic/Suspicious.java
@@ -7,25 +7,19 @@ public class Suspicious extends Guard{
 	public Suspicious(Position pos , String name, char[] moves) {
 		super(pos , name, moves);
 	}
-	
+
 	@Override
 	public void movement() {
 		Random random = new Random();
 		int n = random.nextInt(10) + 1;
 
 		if (n < 3) {
-			if (reverse) {
-				reverse = false;
-			} else { 
-				reverse = true;
+			if (reverse) reverse = false;
+				else reverse = true;
+
+
 			}
-
-		}
-
-		if (reverse) {
-			guardReverseMoves();
-		} else {
 			guardMoves();
 		}
 	}
-}
+
